37e50ecbb4281cce08784c44a33e583ddd0c5d2d
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-08-02 20:19:21 +0500

    Initial commit

---
+++ src/main/java/org/tba/paralleltranslator/ParallelTranslatorApplication.java
@@ -0,0 +1,13 @@
+package org.tba.paralleltranslator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParallelTranslatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParallelTranslatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/tba/paralleltranslator/ParallelTranslatorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tba.paralleltranslator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParallelTranslatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
