5fb5a3f3d5bc93b3ea9c60a22178e574522f3acd
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-08-02 21:13:48 +0500

    feat: added MyMemoryTranslationClient class

--- src/main/java/org/tba/paralleltranslator/ParallelTranslatorApplication.java
+++ src/main/java/org/tba/paralleltranslator/ParallelTranslatorApplication.java
@@ -2,6 +2,8 @@ package org.tba.paralleltranslator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ParallelTranslatorApplication {
@@ -10,4 +12,8 @@ public class ParallelTranslatorApplication {
 		SpringApplication.run(ParallelTranslatorApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
