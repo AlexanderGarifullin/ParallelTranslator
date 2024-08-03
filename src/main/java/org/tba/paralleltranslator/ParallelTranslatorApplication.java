9be77a1d2ce0e14ce89f0689952b2c6e98bc6500
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-08-03 21:58:47 +0500

    feat: added ThreadPool and enable async in the application

--- src/main/java/org/tba/paralleltranslator/ParallelTranslatorApplication.java
+++ src/main/java/org/tba/paralleltranslator/ParallelTranslatorApplication.java
@@ -3,9 +3,14 @@ package org.tba.paralleltranslator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 @SpringBootApplication
+@EnableAsync
 public class ParallelTranslatorApplication {
 
 	public static void main(String[] args) {
@@ -15,5 +20,10 @@ public class ParallelTranslatorApplication {
 	@Bean
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
+	}
+
+	@Bean
+	public ExecutorService executorService() {
+		return Executors.newFixedThreadPool(10);
 	}
 }
