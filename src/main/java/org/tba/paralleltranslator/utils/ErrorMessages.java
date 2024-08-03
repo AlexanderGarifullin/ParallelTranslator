6697a6ac29e0a714eafa31d6ed5fd8e2b699ac66
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-08-03 21:56:20 +0500

    feat: string constants for error handling

---
+++ src/main/java/org/tba/paralleltranslator/utils/ErrorMessages.java
@@ -0,0 +1,8 @@
+package org.tba.paralleltranslator.utils;
+
+
+public class ErrorMessages {
+    public static final String ERROR_TRANSLATION_INTERRUPTED = "Translation interrupted. Please try again.";
+    public static final String ERROR_EXECUTION_EXCEPTION = "Error occurred during translation execution.";
+    public static final String ERROR_WORD_TOO_LONG = "Word exceeds maximum length of 500 bytes.";
+}
