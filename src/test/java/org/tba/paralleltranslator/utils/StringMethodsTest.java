975faa2f7ee18a48f046f80bab1c0303ad52c949
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-08-04 16:28:18 +0500

    test: added unit tests for StringMethods

---
+++ src/test/java/org/tba/paralleltranslator/utils/StringMethodsTest.java
@@ -0,0 +1,45 @@
+package org.tba.paralleltranslator.utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringMethodsTest {
+
+    @Test
+    public void testFindFirstLetterIndex() {
+        assertEquals(0, StringMethods.findFirstLetterIndex("Hello"));
+        assertEquals(2, StringMethods.findFirstLetterIndex("  Hello"));
+        assertEquals(3, StringMethods.findFirstLetterIndex("123a456"));
+        assertEquals(-1, StringMethods.findFirstLetterIndex("123456"));
+        assertEquals(1, StringMethods.findFirstLetterIndex("!a"));
+    }
+
+    @Test
+    public void testFindLastLetterIndex() {
+        assertEquals(4, StringMethods.findLastLetterIndex("Hello"));
+        assertEquals(6, StringMethods.findLastLetterIndex("  Hello"));
+        assertEquals(3, StringMethods.findLastLetterIndex("123a"));
+        assertEquals(-1, StringMethods.findLastLetterIndex("123456"));
+        assertEquals(0, StringMethods.findLastLetterIndex("a!"));
+    }
+
+    @Test
+    public void testGetCleanWorld() {
+        assertEquals("Hello", StringMethods.getCleanWorld("Hello"));
+        assertEquals("Hello", StringMethods.getCleanWorld("  Hello  "));
+        assertEquals("a", StringMethods.getCleanWorld("123a456"));
+        assertEquals("", StringMethods.getCleanWorld("123456"));
+        assertEquals("a", StringMethods.getCleanWorld("!a!"));
+    }
+
+    @Test
+    public void testUniteText() {
+        assertEquals("Hola", StringMethods.uniteText("Hello", "Hola"));
+        assertEquals("  Hola  ", StringMethods.uniteText("  Hello  ", "Hola"));
+        assertEquals("123Hola456", StringMethods.uniteText("123Hello456", "Hola"));
+        assertEquals("123456", StringMethods.uniteText("123456", "Hola"));
+        assertEquals("!231Hola", StringMethods.uniteText("!231Hello", "Hola"));
+        assertEquals("Hola!123", StringMethods.uniteText("Hello!123", "Hola"));
+    }
+}
