00324ae723872adf4375aaa053b00a43b51f311f
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-08-05 18:23:40 +0500

    ref: changed the column type of the table to save the query time

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,5 +3,5 @@ CREATE TABLE api_translation_requests_logs (
     client_ip VARCHAR(45) NOT NULL,
     text TEXT NOT NULL,
     translated_text TEXT NOT NULL,
-    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
