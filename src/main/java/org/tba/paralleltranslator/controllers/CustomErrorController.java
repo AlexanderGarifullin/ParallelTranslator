c6be282bc23a30d000a93e213aa50cf62dd8a638
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-08-06 23:31:29 +0500

    feat: added error page

---
+++ src/main/java/org/tba/paralleltranslator/controllers/CustomErrorController.java
@@ -0,0 +1,14 @@
+package org.tba.paralleltranslator.controllers;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+    @RequestMapping("/error")
+    public String handleError() {
+        // Return the error.html template
+        return "error";
+    }
+}
