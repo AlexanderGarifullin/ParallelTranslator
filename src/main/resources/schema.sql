f6635ada0b67b6d7609db065f3b420eac5075420
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-08-06 17:55:45 +0500

    ref: create table if it is not exists in schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE api_translation_requests_logs (
+CREATE TABLE IF NOT EXISTS api_translation_requests_logs (
     id SERIAL PRIMARY KEY,
     client_ip VARCHAR(45) NOT NULL,
     text TEXT NOT NULL,
